363348f3c8729003c6c5c77e966f506832744cfb
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-09-21 03:25:46 +0000

    Update: Task 7: Ok!

---
+++ api/v1/views/states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Contains the class DBStorage
+"""
+
+from models.state import State 
+from models import storage
+from flask import Flask, jsonify, abort
+from api.v1.views import app_views
+
+
+@app_views.route('/states', strict_slashes=False)
+def lsStates():
+	"""
+
+	"""
+	lstates = []
+	for key, value in storage.all('State').items():
+		lstates.append(value.to_dic())
+	return jsonify(lstates)
+
+@app_views.route('/states/<state_id>', strict_slashes=False)
+def return_obj_stateId(state_id):
+	"""
+	function that return un obj with ID
+	"""
+	obj_st = storage.get('State', state_id)
+	if state_id is None:
+		abort(404)
+	return json.dump(obj_st.to_dict())
