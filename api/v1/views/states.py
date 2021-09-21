4f209a76f2a3738e8cddbbbe20932d80ec37929e
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-09-21 03:28:01 +0000

    update: correct pep8

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,7 +3,7 @@
 Contains the class DBStorage
 """
 
-from models.state import State 
+from models.state import State
 from models import storage
 from flask import Flask, jsonify, abort
 from api.v1.views import app_views
@@ -11,20 +11,21 @@ from api.v1.views import app_views
 
 @app_views.route('/states', strict_slashes=False)
 def lsStates():
-	"""
+    """
 
-	"""
-	lstates = []
-	for key, value in storage.all('State').items():
-		lstates.append(value.to_dic())
-	return jsonify(lstates)
+    """
+    lstates = []
+    for key, value in storage.all('State').items():
+        lstates.append(value.to_dic())
+    return jsonify(lstates)
+
 
 @app_views.route('/states/<state_id>', strict_slashes=False)
 def return_obj_stateId(state_id):
-	"""
-	function that return un obj with ID
-	"""
-	obj_st = storage.get('State', state_id)
-	if state_id is None:
-		abort(404)
-	return json.dump(obj_st.to_dict())
+    """
+    function that return un obj with ID
+    """
+    obj_st = storage.get('State', state_id)
+    if state_id is None:
+        abort(404)
+    return json.dump(obj_st.to_dict())
