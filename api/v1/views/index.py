fccf223b67b474d876c72cce6d771e321f8c0a73
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-09-21 00:45:55 +0000

    Update task 5: Ok!

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -15,3 +15,19 @@ def status():
     Return json status code
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """
+    created endpoint
+    """
+    stts = {
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User')
+    }
+    return jsonify(stts)
